e9c3fc16163b5e8dcee4828450900eaff6993fda
AndreyKapus <kapustnikov@ukr.net>
2023-09-19 18:49:42 +0300

    order-btn

--- modal.js
+++ modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector(".backdrop");
-const modalOpenBtn = document.querySelector(".order-button");
+const modalOpenBtn = document.getElementById("order-btn-header");
+const heroModalOpenBtn = document.getElementById("order-btn-hero");
 const modalCloseBtn = document.querySelector(".modal-btn-close");
 
 const togleModal = () => modal.classList.toggle("is-hidden");
 
 modalOpenBtn.addEventListener("click", togleModal);
 modalCloseBtn.addEventListener("click", togleModal);
+heroModalOpenBtn.addEventListener("click", togleModal);
