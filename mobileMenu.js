b151911ddcedcde93976afd2ee389107be65e962
AndreyKapus <kapustnikov@ukr.net>
2023-09-19 13:43:12 +0300

    mobile-menu

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,8 +1,10 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const mobileMenuOpen = document.querySelector(".burger-btn");
 const mobileMenuClose = document.querySelector(".menu-close");
+const mobileMenuLink = document.querySelector(".menu-nav-list");
 
 const togleMenu = () => mobileMenu.classList.toggle("is-open");
 
 mobileMenuOpen.addEventListener("click", togleMenu);
 mobileMenuClose.addEventListener("click", togleMenu);
+mobileMenuLink.addEventListener("click", togleMenu);
