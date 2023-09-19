cda0ab0657e1c63198c11eb5eab4e715420121fc
AndreyKapus <kapustnikov@ukr.net>
2023-09-19 15:27:15 +0300

    mobile menu

--- mobileMenu.js
+++ mobileMenu.js
@@ -2,9 +2,26 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const mobileMenuOpen = document.querySelector(".burger-btn");
 const mobileMenuClose = document.querySelector(".menu-close");
 const mobileMenuLink = document.querySelector(".menu-nav-list");
+const bodyEl = document.querySelector("body");
 
-const togleMenu = () => mobileMenu.classList.toggle("is-open");
+const togleMenu = () => {
+  mobileMenu.classList.toggle("is-open");
+  if (mobileMenu.classList.contains("is-open")) {
+    bodyEl.classList.add("stopBodyScroll");
+  }
+  if (!mobileMenu.classList.contains("is-open")) {
+    bodyEl.classList.remove("stopBodyScroll");
+  }
+};
 
 mobileMenuOpen.addEventListener("click", togleMenu);
 mobileMenuClose.addEventListener("click", togleMenu);
 mobileMenuLink.addEventListener("click", togleMenu);
+
+// const onBodyScroll = () => {
+//   if (mobileMenu.classList.contains("is-open")) {
+//     body.classList.add("stopBodyScroll");
+//   }
+// };
+
+// onBodyScroll();
